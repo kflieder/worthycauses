fbbb8c3ee9508044e8d1f9d5bcc5ed34982f73e6
Kacey <125841074+kflieder@users.noreply.github.com>
2026-01-13 15:01:59 -0700

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,9 +4,9 @@ import { scrollToSection } from "../lib/scrollTo";
 function HeroSection() {
     
   return (
-    <div id='hero' className='grid grid-cols-1 place-items-end lg:place-items-center lg:grid-cols-5 h-85vh section-anchor text-white lg:text-black'>
-      <div className='inset-0 opacity-45 absolute block lg:hidden z-1 max-h-[95vh]' style={{backgroundImage: "url('/heart.jpg')", backgroundSize: 'cover', backgroundPosition: '80% center'}}/>
-      <div className='relative z-49 col-span-2 xl:ml-15 flex flex-col justify-between max-h-full md:h-1/2 lg:h-full items-center py-4 xl:py-15 bg-black/40 lg:bg-transparent text-center lg:text-left mx-4 xl:mx-0'>
+    <div id='hero' className='grid grid-cols-1 place-items-end lg:place-items-center lg:grid-cols-5 max-h-screen section-anchor text-white lg:text-black'>
+      <div className='inset-0 opacity-45 absolute block lg:hidden z-1 max-h-screen' style={{backgroundImage: "url('/heart.jpg')", backgroundSize: 'cover', backgroundPosition: '80% center'}}/>
+      <div className='relative z-49 col-span-2 xl:ml-15 flex flex-col justify-between max-h-screen md:h-1/2 lg:h-full items-center py-4 xl:py-15 bg-black/40 lg:bg-transparent text-center lg:text-left mx-4 xl:mx-0'>
         <h1 className="text-lg sm:text-xl xl:text-2xl font-bold px-2 leading-10">Helping People and Communities Where It Matters Most</h1>
         <h3 className='text-base sm:text-lg lg:bg-blue-50 bg-transparent rounded-lg p-6'>
           Worthy Causes is a nonprofit organization dedicated to creating meaningful change through education, humanitarian aid, and community-focused programs that uplift lives and open doors to opportunity.
